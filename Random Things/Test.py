1aac4c8044d56b6fc48330f0d8d208bd50b1b09e
Eduardo Luiz Gottems <92766854+EduardoGottems@users.noreply.github.com>
2024-10-18 14:14:31 +0000

    Adicionado a classe "Professores" e atributos do tal

---
+++ Random Things/Test.py
@@ -0,0 +1,7 @@
+class Professor():
+    def __init__(self, nome, dataNascimento, cpf ,numCarTrabalho):
+        self.nome = nome
+        self.dataNascimento = dataNascimento
+        self.cpf = cpf
+        self.numCarTrabalho = numCarTrabalho
+
